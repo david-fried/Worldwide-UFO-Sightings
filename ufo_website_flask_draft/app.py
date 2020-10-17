9985e2b28059a4a137325f2e981badf321bc9fc4
David Fried <david.d.fried@gmail.com>
2020-10-16 22:35:51 -0500

    updated app and database for B Daves

--- ufo_website_flask_draft/app.py
+++ ufo_website_flask_draft/app.py
@@ -18,6 +18,7 @@ Base.prepare(engine, reflect=True)
 ufo_data = Base.classes.ufo_data
 state_stats = Base.classes.state_stats
 merge_again = Base.classes.merge_again
+shape_counts = Base.classes.shape_counts
 
 app = Flask(__name__)
 
@@ -162,6 +163,89 @@ def d3data():
             'light': result[5],
             'triangle': result[6],
             'state_abb': result[7]
+        }
+        
+        big_list.append(obj)
+
+    data=jsonify(big_list)
+    
+    return data
+
+
+########Circle Pack###########
+
+@app.route("/circlepack")
+def circlepack():
+    return render_template('/circlepack.html')
+
+
+########Bar Graph###########
+
+@app.route("/bar")
+def bar():
+    return render_template('/bar.html')
+
+
+@app.route("/bardata")
+def bardata():
+    
+    session = Session(engine)
+
+    from flask import jsonify
+
+    results = session.query(
+    shape_counts.State,
+    shape_counts.Changing,
+    shape_counts.Chevron,
+    shape_counts.Cigar,
+    shape_counts.Circle,
+    shape_counts.Cone,
+    shape_counts.Cross,
+    shape_counts.Cylinder,
+    shape_counts.Diamond,
+    shape_counts.Disk,
+    shape_counts.Egg,
+    shape_counts.Fireball,
+    shape_counts.Flash,
+    shape_counts.Formation,
+    shape_counts.Light,
+    shape_counts.Other,
+    shape_counts.Oval,
+    shape_counts.Rectangle,
+    shape_counts.Sphere,
+    shape_counts.Teardrop,
+    shape_counts.Triangle,
+    shape_counts.Unknown).all()
+
+    session.close()
+
+    big_list = []
+
+    for result in results:
+
+        obj = {
+        'State': result[0],
+        'Changing': result[1],
+        'Chevron': result[2],
+        'Cigar': result[3],
+        'Circle': result[4],
+        'Cone': result[5],
+        'Cross': result[6],
+        'Cylinder': result[7], 
+        'Diamond': result[8],
+        'Disk': result[9],
+        'Egg': result[10],
+        'Fireball': result[11],
+        'Flash': result[12],
+        'Formation': result[13],
+        'Light': result[14],
+        'Other': result[15],
+        'Oval': result[16],
+        'Rectangle': result[17],
+        'Sphere': result[18],
+        'Teardrop': result[19],
+        'Triangle': result[20],
+        'Unknown': result[21]
         }
         
         big_list.append(obj)
